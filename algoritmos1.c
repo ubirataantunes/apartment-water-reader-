afa0cb5e0a5f92885a9194ea6b2bb1a070684691
Ubiratã <ubirata_lucas03@hotmail.com>
2022-05-18 21:32:53 -0300

    College Activity

---
+++ algoritmos1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void main() {
+	int N, apartamento, contadora, apartamentomaiorgasto;
+	float gasto, resultado, maiorgasto;
+		
+	printf("Digite a quantidade de apartamentos que deseja fazer leitura ");
+	scanf("%d", &N);
+	
+	system("cls");
+	
+	for (contadora = 1; contadora <= N; contadora++) {
+		apartamento = contadora;
+		printf("Digite a quantidade de litros de agua gastos pelo apartamento %d\n", apartamento);
+		scanf("%f", &gasto);
+		resultado = gasto/1000;
+		printf("Quantidade de agua em metros cubicos gasta pelo apartamento: %.3f\n\n", resultado);
+			if (maiorgasto < resultado) {
+				apartamentomaiorgasto = apartamento;
+				maiorgasto = resultado;
+			}
+		system("cls");
+	}
+	printf("Apartamento com maior gasto:  %d\n\n", apartamentomaiorgasto);
+	printf("Quantia gasta em metros cubicos de agua pelo apartamento:  %.3f", maiorgasto);
+}
+
+
